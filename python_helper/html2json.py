dc06a8b5f2968217f0df1a2f91a8325a80156ee1
shyu216 <yusihong073@gmail.com>
2024-09-29 00:02:21 +1000

    S

--- python_helper/html2json.py
+++ python_helper/html2json.py
@@ -2,42 +2,42 @@ import json
 from bs4 import BeautifulSoup
 
 def html_to_json(html_content):
+
+    # 不递归则标签处理很麻烦
+    keys = set
+
+
+    def parse_element(element):
+        if element.name == 'a':
+            return {
+                'type': 'link',
+                'addDate': element.get('add_date'),
+                'lastModified': element.get('last_modified'),
+                'title': element.text,
+                'url': element.get('href'),
+                'icon': element.get('icon')
+            }
+        elif element.name == 'h3':
+            return {
+                'type': 'folder',
+                'addDate': element.get('add_date'),
+                'lastModified': element.get('last_modified'),
+                'title': element.text,
+                'children': [parse_element(child) for child in element.find_next_sibling('dl').p.find_all(['a', 'h3'])]
+            }
+        return None
+
     soup = BeautifulSoup(html_content, 'html.parser')
-    bookmarks = []
+    bookmarks = [parse_element(h3) for h3 in soup.dl.p.find_all('h3')]
 
-    def parse_folder(folder):
-        folder_data = {
-            "type": "folder",
-            "addDate": int(folder.h3.get('add_date', 0)) if folder.h3 else 0,
-            "title": folder.h3.get_text() if folder.h3 else "Untitled Folder",
-            "children": []
-        }
-        for child in folder.find_all(['dt', 'dl'], recursive=True):
-            if child.name == 'dt':
-                link = child.find('a')
-                if link:
-                    folder_data["children"].append({
-                        "type": "link",
-                        "addDate": int(link.get('add_date', 0)),
-                        "title": link.get_text(),
-                        "icon": link.get('icon', ''),
-                        "url": link.get('href')
-                    })
-            elif child.name == 'dl':
-                folder_data["children"].append(parse_folder(child))
-        return folder_data
-
-    for folder in soup.find_all('dl', recursive=False):
-        bookmarks.append(parse_folder(folder))
-
-    return json.dumps(bookmarks, indent=4)
+    return json.dumps(bookmarks, ensure_ascii=False, indent=4)
 
 # 从文件中读取HTML书签内容
-with open('./bookmarks.html', 'r', encoding='utf-8') as f:
+with open('./json/bookmarks.html', 'r', encoding='utf-8') as f:
     html_content = f.read()
 
 # 调用函数并打印结果
 json_output = html_to_json(html_content)
 
-with open('./bookmarks.json', 'w', encoding='utf-8') as f:
+with open('./json/pintree.json', 'w', encoding='utf-8') as f:
     f.write(json_output)
